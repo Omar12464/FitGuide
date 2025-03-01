0196085d6342792b6c5eb9f4c0ed218de950a5e6
Omar Ebrahim Mohamed Khalil <200012464@must.edu.eg>
2025-03-02 01:03:04 +0200

    Creating DataBase

---
+++ Core/WorkOutPlan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class WorkOutPlan
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int NumberOfDays { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+
+    }
+}
